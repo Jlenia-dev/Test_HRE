eff68bb43f2f9afc70b1bf8700bbca707f042816
UX_lab <jleniagabrielli@proton.me>
2026-09-23 22:21:42 +0200

    Sostituisco task-tabs.html e tasks.js con i file aggiornati forniti

--- tasks.js
+++ tasks.js
@@ -40,7 +40,7 @@ const TASKS = {
     title: "Task 3",
     scenario: "Immagina di aver ricevuto un nuovo documento che deve essere inserito nel sistema.",
     activities: [
-      "Il documento: <li>proviene dalla banca [banca mittente - 03599];</li><li> è datato [10/10/2026]; <li>è associato all’azienda [03599]; <li>si tratta di un documento relativo al <strong>Cedolino paga</strong></li>; <li>è riferito a <strong> Antonina Alboni</strong>, <strong> matricola INPS</strong> </li>; <li>il file da utilizzare è <strong> '3599 CEDO 2026 Settembre.pdf'</strong></li>. <br> <strong>Inserisci il documento nel sistema utilizzando le informazioni che ti sono state fornite </strong>e assicurati che l’operazione venga completata correttamente."
+      "<p>Il documento:</p><ul><li>proviene dalla banca [banca mittente - 03599];</li><li>è datato [10/10/2026];</li><li>è associato all’azienda [03599];</li><li>si tratta di un documento relativo al <strong>Cedolino paga</strong>;</li><li>è riferito a <strong>Antonina Alboni</strong>, <strong>matricola INPS</strong>;</li><li>il file da utilizzare è <strong>'3599 CEDO 2026 Settembre.pdf'</strong>.</li></ul><p><strong>Inserisci il documento nel sistema utilizzando le informazioni che ti sono state fornite</strong> e assicurati che l’operazione venga completata correttamente.</p>"
     ],
     survey: "https://forms.cloud.microsoft/e/30K5b4qWCP"
   },
